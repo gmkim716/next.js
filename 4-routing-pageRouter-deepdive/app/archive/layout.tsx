1e43fcdae458f79e91fa5bf9a532bdddb6e8449f
Jayden, Gyeongmin Kim <starlight3775@gmail.com>
2024-05-20 00:28:13 +0900

    feat: catch-all-router

---
+++ 4-routing-pageRouter-deepdive/app/archive/layout.tsx
@@ -0,0 +1,15 @@
+export default function ArchiveLayout({
+  archive,
+  latest,
+}: {
+  archive: any;
+  latest: any;
+}) {
+  return (
+    <div>
+      <h1>News Archive</h1>
+      <section id="archive-filter">{archive}</section>
+      <section id="latest-filter">{latest}</section>
+    </div>
+  );
+}
