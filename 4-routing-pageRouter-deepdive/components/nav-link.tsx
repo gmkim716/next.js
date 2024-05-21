6c9ae25ef9372b90a087acf854dc4cc0221c9053
Jayden, Gyeongmin Kim <starlight3775@gmail.com>
2024-05-21 17:37:26 +0900

    fix: update chapter4

--- 4-routing-pageRouter-deepdive/components/nav-link.tsx
+++ 4-routing-pageRouter-deepdive/components/nav-link.tsx
@@ -4,11 +4,12 @@ import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 export default function NavLink({ href, children }: { href: string; children: React.ReactNode }) {
-
   const path = usePathname();
 
+  // console.log('path', path)
+
   return (
-    <Link href="/news" className={path.startsWith(href) ? 'active' : undefined}>
+    <Link href={href} className={path.startsWith(href) ? 'active' : undefined}>
       {children}
     </Link>
   )
